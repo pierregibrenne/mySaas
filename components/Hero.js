83bc584811b46935bf0fd820bd476ee834ebe489
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-05-31 18:34:49 +0200

    firstCommit

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import TestimonialsAvatars from "./TestimonialsAvatars";
 import config from "@/config";
-
+import ButtonLead from "./ButtonLead";
 const Hero = () => {
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
