54409975711e6510b8bbb09816ff803dc24db30b
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-06-02 11:51:19 +0200

    feat:testButton OK ✅

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,10 @@
+import BetterIcon from "@/components/BetterIcon";
 import ButtonAccount from "@/components/ButtonAccount";
 import ButtonCheckout from "@/components/ButtonCheckout";
+import ButtonGradient from "@/components/ButtonGradient";
+import ButtonLead from "@/components/ButtonLead";
+import ButtonPopover from "@/components/ButtonPopover";
+import ButtonSupport from "@/components/ButtonSupport";
 import config from "@/config";
 
 export const dynamic = "force-dynamic";
@@ -13,7 +18,11 @@ export default async function Dashboard() {
         <h1 className="text-3xl md:text-4xl font-extrabold">
           Subscribe to get access:
         </h1>
-
+    <ButtonLead/>
+    <ButtonGradient/>
+    <ButtonPopover/>
+    <ButtonSupport/>
+    <BetterIcon/>
         <ButtonCheckout
           mode="subscription"
           priceId={config.stripe.plans[0].priceId}
--- components/Hero.js
+++ components/Hero.js
@@ -22,11 +22,10 @@ const Hero = () => {
         </a>
 
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-          Ship your startup in days, not weeks
+          Make your custom form, view your data
         </h1>
         <p className="text-lg opacity-80 leading-relaxed">
-          The NextJS boilerplate with all you need to build your SaaS, AI tool,
-          or any other web app. From idea to production in 5 minutes.
+          Make your custom form, send a link your community, and you see a data in your dashboard 
         </p>
         <button className="btn btn-primary btn-wide">
           Get {config.appName}
